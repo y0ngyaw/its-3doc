81c9affbfe53b13f15965a901f52b337f9877a88
y0ngyaw <yongyawyeo98@gmail.com>
2018-03-15 23:10:23 +0800

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,7 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { host: 'localhost:3000', protocol: 'https' }
+  config.action_mailer.default_url_options = { host: 'its-3doc-production-env.8fgpa9wwnj.ap-southeast-1.elasticbeanstalk.com', protocol: 'http' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
         :address              => 'smtp.gmail.com',
@@ -76,6 +76,7 @@ Rails.application.configure do
         :authentication       => 'login',
         :enable_starttls_auto => true
     }
+  config.action_mailer.asset_host = 'its-3doc-production-env.8fgpa9wwnj.ap-southeast-1.elasticbeanstalk.com'
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
