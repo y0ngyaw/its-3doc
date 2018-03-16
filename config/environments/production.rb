95b047268ba9094c15640c4a207e97254ceaf742
y0ngyaw <yongyawyeo98@gmail.com>
2018-03-16 12:37:39 +0800

    Update production smtp settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,6 +67,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options = { host: 'its-3doc-production-env.8fgpa9wwnj.ap-southeast-1.elasticbeanstalk.com', protocol: 'http' }
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true  
   config.action_mailer.smtp_settings = {
         :address              => 'smtp.gmail.com',
         :port                 => 587,
