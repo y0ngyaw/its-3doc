465ab27d5bc3b296a5a43671669028368d4a8260
y0ngyaw <yongyawyeo98@gmail.com>
2018-03-20 15:43:19 +0800

    Add nginx conf file in .ebextensions

--- config/application.rb
+++ config/application.rb
@@ -21,12 +21,5 @@ module Its3doc
     		ENV[key.to_s] = value
     	end  if File.exists?(env_file)
     end
-
-    config.middleware.insert_before 0, Rack::Cors do
-        allow do
-            origins '*'
-            resource '*', :headers => :any, :methods => [:get, :post, :options]
-        end
-    end
   end
 end
