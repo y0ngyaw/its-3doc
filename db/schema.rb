bb82a4eee02d0a62a17797ffb28151aee2cccf43
y0ngyaw <yongyawyeo98@gmail.com>
2018-04-10 20:31:14 +0800

    Update Sponsor table admin column default value

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180409054823) do
+ActiveRecord::Schema.define(version: 20180410122802) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -64,7 +64,7 @@ ActiveRecord::Schema.define(version: 20180409054823) do
   create_table "sponsors", force: :cascade do |t|
     t.string "email"
     t.string "password_digest"
-    t.boolean "admin"
+    t.boolean "admin", default: false
     t.string "remember_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
